9349d8786fd1a2e9b3f701b4df7fa484a6fa4668
alexg1021 <gutierrez.lx@gmail.com>
2016-04-22 17:04:41 +0000

    remove bc

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ var toDoArray = JSON.parse(localStorage.listItems || null) || [];
 form.submit.disabled = true;
 
 /* ================= Event Listeners ====================== */
-//disables submit button if text area is empty
+//d
 form.addEventListener('input', function () {
     if (form.toDoItem.value.length > 0) {
         form.submit.disabled = false;
@@ -18,7 +18,6 @@ form.addEventListener('input', function () {
     }
 });
 
-//listens for a submit event and adds a new toDoItem
 form.addEventListener('submit', function (evt) {
     evt.preventDefault();
 
